3b7fb6f904cc4aa6d2548e361545109e03d7bf58
Soljibae <bch5069@gmail.com>
2025-08-07 17:54:59 +0900

    Add templated Vector class

--- DJS_Engine/DJS_Engine/Private/DJS_Math.cpp
+++ DJS_Engine/DJS_Engine/Private/DJS_Math.cpp
@@ -1,19 +1,1 @@
 #include "DJS_Math.h"
-
-Vec2::Vec2(): x(0.f), y(0.f) {}
-
-Vec2::Vec2(float value) : x(value), y(value) {}
-
-Vec2::Vec2(float x, float y) : x(x), y(y) {}
-
-Vec3::Vec3() : x(0.f), y(0.f), z(0.f) {}
-
-Vec3::Vec3(float value) : x(value), y(value), z(value) {}
-
-Vec3::Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
-
-Vec4::Vec4() : x(0.f), y(0.f), z(0.f), w(0.f) {}
-
-Vec4::Vec4(float value) : x(value), y(value), z(value), w(value) {}
-
-Vec4::Vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
--- DJS_Engine/DJS_Engine/Public/DJS_Math.h
+++ DJS_Engine/DJS_Engine/Public/DJS_Math.h
@@ -1,35 +1,82 @@
 #pragma once
 
-struct Vec2 {
+template<typename T>
+class tvec2{
+public:
     union {
-        struct { float x, y; };
-        float data[2];
+        struct { T x, y; };
+        T data[2];
     };
 
-    Vec2();
-    Vec2(float value);
-    Vec2(float x, float y);
+    tvec2() : x(0), y(0) {}
+    tvec2(T value) : x(value), y(value) {}
+    tvec2(T x, T y) : x(x), y(y) {}
+
+    size_t size() const {
+        return 2;
+    }
+
+    T& operator[](int i) { return data[i]; }
+    const T& operator[](int i) const { return data[i]; }
 };
 
-struct Vec3 {
+using vec2 = tvec2<float>;
+using dvec2 = tvec2<double>;
+using ivec2 = tvec2<int>;
+
+template<typename T>
+class tvec3 {
+public:
     union {
-        struct { float x, y, z; };
-        float data[3];
+        struct { T x, y, z; };
+        T data[3];
     };
 
-    Vec3();
-    Vec3(float value);
-    Vec3(float x, float y, float z);
+    tvec3() : x(0), y(0), z(0) {}
+    tvec3(T value) : x(value), y(value), z(value) {}
+    tvec3(T x, T y, T z) : x(x), y(y), z(z) {}
+
+    size_t size() const {
+        return 3;
+    }
+
+    T& operator[](int i) { return data[i]; }
+    const T& operator[](int i) const { return data[i]; }
 };
 
-struct Vec4 {
+using vec3 = tvec3<float>;
+using dvec3 = tvec3<double>;
+using ivec3 = tvec3<int>;
+
+template<typename T>
+class tvec4 {
+public:
     union {
-        struct { float x, y, z, w; };
-        float data[4];
+        struct { T x, y, z, w; };
+        T data[4];
     };
 
-    Vec4();
-    Vec4(float value);
-    Vec4(float x, float y, float z, float w);
+    tvec4() : x(0), y(0), z(0), w(0) {}
+    tvec4(T value) : x(value), y(value), z(value), w(value) {}
+    tvec4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+
+    size_t size() const {
+        return 4;
+    }
+
+    T& operator[](int i) { return data[i]; }
+    const T& operator[](int i) const { return data[i]; }
 };
 
+using vec4 = tvec4<float>;
+using dvec4 = tvec4<double>;
+using ivec4 = tvec4<int>;
+
+template<typename T>
+void VecNegate(T& result, const T& vec0)
+{
+    for (size_t i = 0; i < result.size(); i++)
+    {
+        result[i] = vec0[i] * -1;
+    }
+}
